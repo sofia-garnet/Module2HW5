e379cb392f2f8feb017dce97c767ac3e36209584
AngrySonya <Photographersofi@gmail.com>
2021-07-19 23:43:33 +0300

    add project

---
+++ Module2HW5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Module2HW5.Abstraction;
+
+namespace Module2HW5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IFileService, FileService>()
+                .AddSingleton<ILogger, Logger>()
+                .AddTransient<Actions>()
+                .AddTransient<Starter>().BuildServiceProvider();
+            var starter = serviceProvider.GetService<Starter>();
+            starter.Run();
+        }
+    }
+}
